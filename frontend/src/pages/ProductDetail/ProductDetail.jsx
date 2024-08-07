f572df4f4e3c5be98b4bbf64da209845cf4b45d0
olyflower <2013.olga.yudina@gmail.com>
2024-08-07 10:32:13 +0300

    redux

--- frontend/src/pages/ProductDetail/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail/ProductDetail.jsx
@@ -1,92 +1,95 @@
-import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-
 import Raiting from "../../components/Rating/Rating";
-import axios from "axios";
+import Loader from "../../components/Loader/Loader";
+import Message from "../../components/Message/Message";
+import { useGetProductDetailsQuery } from "../../slices/productsSlice";
 import img from "../../assets/img1.jpg";
 
 const ProductDetail = () => {
-	const [product, setProduct] = useState({});
 	const { id: productId } = useParams();
-
-	useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get(`/api/products/${productId}`);
-			setProduct(data);
-		};
-
-		fetchProduct();
-	}, [productId]);
+	const {
+		data: product,
+		isLoading,
+		error,
+	} = useGetProductDetailsQuery(productId);
 
 	return (
 		<>
 			<Link className="btn btn-light my-3" to="/">
 				Go Back
 			</Link>
-			<Row>
-				<Col md={5}>
-					<Image
-						src={img}
-						alt={product.name}
-						width={300}
-						height={400}
-					/>
-				</Col>
-				<Col md={4}>
-					<ListGroup variant="flush">
-						<ListGroup.Item>
-							<h3>{product.name}</h3>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Raiting
-								value={product.rating}
-								text={`${product.numReviews} reviwes`}
-							/>
-						</ListGroup.Item>
-						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-						<ListGroup.Item>
-							Description: {product.description}
-						</ListGroup.Item>
-					</ListGroup>
-				</Col>
-				<Col md={3}>
-					<Card>
+			{isLoading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error?.data.message || error.error}</Message>
+			) : (
+				<Row>
+					<Col md={5}>
+						<Image
+							src={img}
+							alt={product.name}
+							width={300}
+							height={400}
+						/>
+					</Col>
+					<Col md={4}>
 						<ListGroup variant="flush">
 							<ListGroup.Item>
-								<Row>
-									<Col>Price:</Col>
-									<Col>
-										<strong>${product.price}</strong>
-									</Col>
-								</Row>
+								<h3>{product.name}</h3>
 							</ListGroup.Item>
 							<ListGroup.Item>
-								<Row>
-									<Col>Status:</Col>
-									<Col>
-										<strong>
-											{product.CountInStock > 0
-												? "In Stock"
-												: "Out of Stock"}
-										</strong>
-									</Col>
-								</Row>
+								<Raiting
+									value={product.rating}
+									text={`${product.numReviews} reviwes`}
+								/>
 							</ListGroup.Item>
 							<ListGroup.Item>
-								<Button
-									className="'btn-clock"
-									type="button"
-									disabled={product.CountInStock === 0}
-								>
-									Add to Cart
-								</Button>
+								Price: ${product.price}
+							</ListGroup.Item>
+							<ListGroup.Item>
+								Description: {product.description}
 							</ListGroup.Item>
 						</ListGroup>
-					</Card>
-				</Col>
-			</Row>
+					</Col>
+					<Col md={3}>
+						<Card>
+							<ListGroup variant="flush">
+								<ListGroup.Item>
+									<Row>
+										<Col>Price:</Col>
+										<Col>
+											<strong>${product.price}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<Row>
+										<Col>Status:</Col>
+										<Col>
+											<strong>
+												{product.CountInStock > 0
+													? "In Stock"
+													: "Out of Stock"}
+											</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<Button
+										className="'btn-clock"
+										type="button"
+										disabled={product.CountInStock === 0}
+									>
+										Add to Cart
+									</Button>
+								</ListGroup.Item>
+							</ListGroup>
+						</Card>
+					</Col>
+				</Row>
+			)}
 		</>
 	);
 };
